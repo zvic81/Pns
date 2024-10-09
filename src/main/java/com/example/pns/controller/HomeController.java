fbd9bf9f48c9267132686b52c483a4ae77b0cbac
vic <zvic@list.ru>
2024-10-09 13:36:50 +0300

    feat: End stage 1: WebServer(Spring boot)+Page Reader(Thymeleaf) Add notification.html, styles.css. Page for reading messages by users is almost ready

--- src/main/java/com/example/pns/controller/HomeController.java
+++ src/main/java/com/example/pns/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.example.pns.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -11,4 +17,17 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping("/notifications")
+    public String showNotification(Model model){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String currentTime = LocalDateTime.now().format(formatter);
+        model.addAttribute("currentTime", currentTime);
+        List<String> messages = new ArrayList<>();
+        messages.add("Test1 message");
+        messages.add("Test111 message. Tomcat started on port 8080 (http) with context path '/' 2024-10-09T13:23:44.137+03:00  INFO 293062 --- [Pns] [  restartedMain] w.s.c.ServletWebServerApplicationContext : Root WebApplicationContext: initialization completed in 1425 ms");
+        messages.add("Test222 message");
+        messages.add("Test3 message. Starting Servlet engine: [Apache Tomcat/10.1.30]. Initializing Servlet 'dispatcherServlet'");
+        model.addAttribute("messages", messages);
+        return "notification";
+    }
 }
