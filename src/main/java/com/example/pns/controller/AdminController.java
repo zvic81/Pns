1786e5a284a8e873ddec4e92636149f18c5dad60
vic <zvic@list.ru>
2024-10-11 13:45:59 +0300

    feat: Made admin page ang controller for edit messages. Stage 2 finished

--- src/main/java/com/example/pns/controller/AdminController.java
+++ src/main/java/com/example/pns/controller/AdminController.java
@@ -1,33 +1,34 @@
 package com.example.pns.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class HomeController {
-    @GetMapping("/")
-    public String HomePage(){
-
-        return "home";
-    }
-
-    @GetMapping("/notifications")
-    public String showNotification(Model model){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String currentTime = LocalDateTime.now().format(formatter);
-        model.addAttribute("currentTime", currentTime);
-        List<String> messages = new ArrayList<>();
+public class AdminController {
+    private static List<String> messages = new ArrayList<>();
+    static {
         messages.add("Test1 message");
         messages.add("Test111 message. Tomcat started on port 8080 (http) with context path '/' 2024-10-09T13:23:44.137+03:00  INFO 293062 --- [Pns] [  restartedMain] w.s.c.ServletWebServerApplicationContext : Root WebApplicationContext: initialization completed in 1425 ms");
         messages.add("Test222 message");
         messages.add("Test3 message. Starting Servlet engine: [Apache Tomcat/10.1.30]. Initializing Servlet 'dispatcherServlet'");
-        model.addAttribute("messages", messages);
-        return "notification";
     }
+    @GetMapping("/admin/edit_messages")
+    public String showEditMessagePage(Model model){
+        model.addAttribute("messages", messages);
+        return "edit_messages";
+    }
+
+    @PostMapping("/admin/edit_messages")
+    public String saveEditMessagePage(@RequestParam("messages") List<String> updatedMessages){
+        messages = updatedMessages;
+        return "redirect:/admin/edit_messages";
+    }
+
+
 }
